444ba3f08b66bea40f8ea6a55333e471abec5c07
Bruno <bberastain@gmail.com>
2021-08-28 14:29:39 -0700

    preferredContact column

--- database/schema.sql
+++ database/schema.sql
@@ -7,6 +7,7 @@ CREATE TABLE IF NOT EXISTS profile (
   homePhone INT,
   mobile INT,
   fax INT,
+  preferredContact INT DEFAULT 0,
   city VARCHAR(75) NOT NULL,
   state VARCHAR(5) NOT NULL,
   zip INT,
