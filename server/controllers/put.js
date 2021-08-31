e05714f100f1a5e6e4203ad26a35385c9d4ecf03
Bruno <bberastain@gmail.com>
2021-08-31 16:37:31 -0700

    removed debugger

--- server/controllers/put.js
+++ server/controllers/put.js
@@ -2,7 +2,6 @@ const { put } = require('../models');
 
 module.exports = {
   profile: async(req, res) => {
-    debugger;
     try {
       let data = await put.profile(req.body);
       res.send(data);
