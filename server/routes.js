6b24d7cc5186de627007672cb25f78f11b2b7100
Sam Schnaible II <sam@Sams-MacBook-Pro.local>
2021-08-31 12:20:47 -0700

    update for merge w/ main

--- server/routes.js
+++ server/routes.js
@@ -2,7 +2,7 @@ const controller = require('./controllers');
 const router = require('express').Router();
 
 router.get('/listings/offers', controller.get.offers);
-// router.post('/listings/offers', controller.post.offers);
+router.post('/listings/offers', controller.post.offers);
 router.get('/profile', controller.get.profile);
 router.get('/listings/requests', controller.get.requests);
 // router.get('/listings/requests', controller.get.requests);
