fb733bf8069c5ad6f31d68271416a52460aced7b
Bruno <bberastain@gmail.com>
2021-09-02 12:15:21 -0700

    refactored POST for offers and requests

--- server/models/helpers.js
+++ server/models/helpers.js
@@ -1,3 +1,5 @@
+const db = require('../../database');
+
 module.exports = {
   photoDefault: (category) => {
     if (!category) return '';
@@ -8,5 +10,28 @@ module.exports = {
       bills: 'https://res.cloudinary.com/fearnomore/image/upload/v1630540922/fearnomore/sharon-mccutcheon-tn57JI3CewI-unsplash_ov3k4n.jpg',
     };
     return defaults[category];
+  },
+  postListing: async (args) => {
+    try {
+      // debugger;
+      let result = await db.none(
+        `INSERT INTO posts(
+          id,
+          user_id,
+          requestType,
+          category,
+          title,
+          body,
+          date, photo)
+        VALUES (
+          (SELECT MAX(id) FROM posts) + 1,
+          (SELECT id FROM profile WHERE email=$1),
+          $2, $3, $4, $5, $6, $7
+        )`, [...args]);
+        return;
+    } catch(err) {
+      console.log(err)
+      return err;
+    }
   }
 }
