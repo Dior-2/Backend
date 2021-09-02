0eb67571902d65191a4a9f1b656a164f78ee142b
Bruno <bberastain@gmail.com>
2021-09-02 14:15:16 -0700

    reorder comments by timestamp

--- server/models/get.js
+++ server/models/get.js
@@ -32,7 +32,7 @@ module.exports = {
   comments: async({ post_id, thread_id }) => {
     try {
       let data = await db.query(`SELECT json_agg(threads) FROM (
-        SELECT username, body, post_id, thread_id, date FROM comments WHERE post_id=$1 ORDER BY date DESC) AS threads GROUP BY thread_id`, [post_id, thread_id]);
+        SELECT username, body, post_id, thread_id, date AS timestamp FROM comments WHERE post_id=$1 ORDER BY timestamp) AS threads GROUP BY thread_id`, [post_id, thread_id]);
       let results = [];
       for (var i = 0; i < data.length; i++) {
         results.push(data[i].json_agg);
